b14ea950108aee81d2b680ddf76e63985745c8ca
kerlyn <kerlynh@gmail.com>
2024-03-25 16:11:06 -0300

    test: test

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+      <WebView
+      style={styles.container}
+      source={{ uri: 'https://nextjs-landing-page-five-steel.vercel.app/' }}
+    />
+    // </View>
   );
 }
 
